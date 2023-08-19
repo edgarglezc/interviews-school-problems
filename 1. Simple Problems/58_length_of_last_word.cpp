241a2e8b00347145461517a2ceef53b362e39e30
Edgar Gonzalez <alejgc99@gmail.com>
2023-08-18 21:10:29 -0600

    last word length

--- 1. Simple Problems/58_length_of_last_word.cpp
+++ 1. Simple Problems/58_length_of_last_word.cpp
@@ -1,15 +1,31 @@
 #include <bits/stdc++.h>
+#include <algorithm> 
+#include <cctype>
+#include <locale>
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
+        int currentWordLength = 0;
+        int lastWordLength = 0;
+        for(char c : s) {
+            if(c == ' ') currentWordLength = 0;
+            else {
+                currentWordLength++;
+                lastWordLength = currentWordLength;
+            }
+        }
+        return lastWordLength;
     }
 };
 
 int main() {
-    
+    Solution sol;
+
+    cout << sol.lengthOfLastWord("Hello World") << endl;    
+    cout << sol.lengthOfLastWord("   fly me   to   the moon  ") << endl;
+    cout << sol.lengthOfLastWord("luffy is still joyboy") << endl;
 
     return 0;
 }
